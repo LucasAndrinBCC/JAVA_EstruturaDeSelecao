693f02baa2051383131b51766f7a4d8ec8651629
LucasAndrinBCC <lucas.andrin.ifc.riodosul@gmail.com>
2022-08-23 09:29:33 -0300

    Finaliza Exercícios

--- src/Exe21.java
+++ src/Exe21.java
@@ -1,13 +1,34 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 //Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma
 //é uma data válida.
 public class Exe21 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		Scanner entrada = new Scanner(System.in);
 		
+		System.out.print("Entre com uma data no formato dd/mm/aaaa: ");		
+		String dataInformada = entrada.nextLine();
+		
+		if (dataInformada.length() != "dd/mm/aaaa".length()) {
+			
+			System.out.println("Data informada inválida! Tamanho da data errático!");
+			
+		} else if (dataInformada.charAt(2) != '/' || dataInformada.charAt(5) != '/') {
+			
+			System.out.println("Data informada inválida! Não foram encontradas barras nas posições corretas");
+			
+		} else {
+			SimpleDateFormat diaMesAno = new SimpleDateFormat("dd/MM/yyyy");
+			
+			Date dataFormatada = diaMesAno.parse(dataInformada);
+			
+			System.out.println("Data formatada final: " + diaMesAno.format(dataFormatada));
+		}
 		
 		
 		entrada.close();
--- src/Exe31.java
+++ src/Exe31.java
@@ -1,14 +1,62 @@
 import java.util.Scanner;
 
 public class Exe31 {
+	
+	private static final int FILE_DUPLO = 1;
+	private static final int ALCATRA = 2;
+	private static final int PICANHA = 3;
+	
 
 	public static void main(String[] args) {
 		
 		Scanner entrada = new Scanner(System.in);
 		
+		System.out.print("Entre com o tipo de carne: ");
+		int tipo = entrada.nextInt();
 		
+		System.out.print("Entre com o peso da carne: ");
+		float kg = entrada.nextFloat();
+		
+		System.out.println("Foi utilizado  o cartão Tabajara? ");
+		boolean cartao = entrada.nextBoolean();
+		
+		String tipoTexto = "Error";
+		float valor = 0;
+		if (tipo == FILE_DUPLO) {
+			tipoTexto = "File Duplo";
+			valor = retornaValor(kg, 4.9f, 5.8f);
+		} else if (tipo == ALCATRA) {
+			tipoTexto = "Alcatra";
+			valor = retornaValor(kg, 5.9f, 6.8f);
+		} else if (tipo == PICANHA) {
+			tipoTexto = "Picanha";
+			valor = retornaValor(kg, 6.9f, 7.8f);
+		}
+		
+		float valorFinal = valor;
+		
+		if (cartao) {
+			valorFinal *= 0.95f;
+		}
+		
+		System.out.println("Tipo: " + tipoTexto);
+		System.out.println("Quantidade: " + kg + "kg");
+		System.out.println("Preço Total: R$" + valor);
+		System.out.println("Tipo de Pagamento: " + (cartao ? "Cartão Tabajara" : "Sem Cartão Tabajara"));
+		System.out.println("Desconto: R$" + (valor - valorFinal));
+		System.out.println("Valor Final: R$" + valor);
 		
 		entrada.close();
+	}
+	
+	public static float retornaValor(float kg, float menorQue5, float maiorQue5) {
+	
+		if (kg < 5) {
+			return kg * menorQue5;
+		} else {
+			return kg * maiorQue5;
+		}
+		
 	}
 
 }
